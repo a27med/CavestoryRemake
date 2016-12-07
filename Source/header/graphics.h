c77f44b31a31aff366e843027ee2bd109de2b102
Ahmed Hamdy <a27med@gmail.com>
2016-12-07 01:02:33 -0800

    CavestoryRemake
    Remaking Cavestory with limeoats

---
+++ Source/header/graphics.h
@@ -0,0 +1,20 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+struct SDL_Window;
+struct SDL_Renderer;
+
+class Graphics {
+public:
+	// constructor: create the window and initialize
+	Graphics();
+	// deconstructor
+	~Graphics();
+private:
+	// pointer to SDL_Window: where we are going to draw
+	SDL_Window* _window;
+	// pointer to SDL_Renderer: does the drawing to the window
+	SDL_Renderer* _renderer;
+};
+
+#endif
